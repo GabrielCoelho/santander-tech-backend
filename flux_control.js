6e9fbc5a6a29034d220d177bc34ff7ea852e0657
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 18:15:56 -0300

    Flux control ex08

--- flux_control.js
+++ flux_control.js
@@ -1,8 +1,8 @@
 const number1 = 100;
 const number2 = 20;
 
-// Any true will outputs true
-console.log(true || true);
-console.log(true || false);
-console.log(false || true);
-console.log(false || false);
+// Only both true wil output true
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
