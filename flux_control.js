e4da8c8ec22bfc2f0288df8d3eff7c2ee9dd8243
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 18:13:11 -0300

    Flux control ex06

--- flux_control.js
+++ flux_control.js
@@ -1,9 +1,10 @@
 const number1 = 100;
 const number2 = 20;
 
-// `==` doesn't compare types.
-if (number2 === number1) {
-  console.log(`${number2} is equal to ${number1}`);
-} else {
-  console.log(`${number2} is not equal to ${number1}`);
+if (true) {
+  console.log("Always appears");
+}
+
+if (false) {
+  console.log("Never appears");
 }
