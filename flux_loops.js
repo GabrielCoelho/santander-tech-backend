7cbb98b23c1ec8f2ee65bc92bee7619e2f2667b0
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 18:32:23 -0300

    Flux loops ex01

---
+++ flux_loops.js
@@ -0,0 +1,9 @@
+const names = ["Joana", "Emily", "Clovis", "Gerald"];
+
+const numberOfNames = 4;
+let rounds = 0;
+
+while (rounds < numberOfNames) {
+  console.log("Olá ", names[rounds]);
+  rounds += 1;
+}
