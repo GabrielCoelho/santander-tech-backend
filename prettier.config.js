c6112c33dc4390542b10d66fccf7b882d46ffcc9
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 19:21:26 -0300

    Configuration for Formatter

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
-const config = {
-  trailingComma: "es5",
-  tabWidth: 2,
-  semi: true,
-  singleQuote: false,
-};
-
-export default config;
+module.exports = [
+  {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: true,
+    singleQuote: false,
+  },
+];
