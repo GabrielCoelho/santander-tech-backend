95ccd57f9342fc07fc8f7ab143c583821d598484
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 18:04:49 -0300

    Flux control ex01

---
+++ flux_control.js
@@ -0,0 +1,10 @@
+const number1 = 100;
+const number2 = 20;
+
+if (number1 > 1) {
+  console.log("This number is greater than 1");
+}
+
+if (number1 > 200) {
+  console.log("This number is greater than 200");
+}
