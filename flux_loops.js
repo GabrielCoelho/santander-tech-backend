5d075a601b008c018e219124f6c665f11002c006
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 20:02:25 -0300

    Flux loops ex07

--- flux_loops.js
+++ flux_loops.js
@@ -1,5 +1,5 @@
 const numberList = [25, 88, 61, 123, 143, 232, 10, 11, 52];
 
-for (let index = 0; index < numberList.length; index++) {
-  console.log(numberList[index]);
+for (const el of numberList) {
+  console.log(el);
 }
