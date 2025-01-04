b17da79b5e026fc4cd70b1e0bd5fa90ff338c656
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 18:20:52 -0300

    Flux control ex09

--- flux_control.js
+++ flux_control.js
@@ -1,8 +1,1 @@
-const number1 = 100;
-const number2 = 20;
-
-// Only both true wil output true
-console.log(true && true);
-console.log(true && false);
-console.log(false && true);
-console.log(false && false);
+console.log(!![], !!{}, !!1, !!"asoteu");
