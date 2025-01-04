bfbeb25f659074329cc8526df36b4ecefa593dc4
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 18:10:51 -0300

    Flux control ex05

--- flux_control.js
+++ flux_control.js
@@ -1,6 +1,9 @@
 const number1 = 100;
 const number2 = 20;
 
-if (number2 < number1) {
-  console.log(`${number2} is lower than ${number1}`);
+// `==` doesn't compare types.
+if (number2 === number1) {
+  console.log(`${number2} is equal to ${number1}`);
+} else {
+  console.log(`${number2} is not equal to ${number1}`);
 }
