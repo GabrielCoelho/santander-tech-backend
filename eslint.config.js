650a47e752ae3b537897cdf72b2e0b9b213a5937
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 19:06:54 -0300

    Configuration for Linter

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    rules: {
+      semi: ["error", "always"],
+      "no-unused-vars": "error",
+      indent: ["error", 2],
+      "prefer-const": "error",
+    },
+  },
+];
