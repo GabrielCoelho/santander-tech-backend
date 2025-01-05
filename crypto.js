85612bc67f2516dc6dca7c01abb3ca9f1439c09c
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-05 11:35:40 -0300

    Crypto ex05

--- crypto.js
+++ crypto.js
@@ -1,5 +1,8 @@
 const crypt = require("node:crypto");
 
-const key = crypt.randomBytes(12).toString("ascii");
+const key = crypt.randomBytes(12).toString("hex");
 
 console.log(key);
+
+const uuid = crypt.randomUUID();
+console.log(uuid);
