f8d19c6107930b86ef2cbda4c22955019dc77daf
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-04 22:26:07 -0300

    Searching a DNS/IP requisitions

--- dns.js
+++ dns.js
@@ -2,10 +2,36 @@ const dns = require("node:dns");
 
 const searchedUrl = "google.com";
 
-const ipv4 = dns.resolve4(searchedUrl, (err, addresses) => {
-  if (err) {
-    console.error("URL not found");
-    return;
-  }
-  console.log({ addresses });
-});
+//const ipv4 = dns.resolve4(searchedUrl, (err, addresses) => {
+//  if (err) {
+//    console.error("URL not found");
+//    return;
+//  }
+//  console.log({ addresses });
+//});
+
+async function bootstrap() {
+  console.time("Searching a URL");
+  const addresses = await dns.promises.resolve4(searchedUrl);
+
+  const nameServers = await dns.promises.resolveNs(searchedUrl);
+  console.log(addresses, "", nameServers);
+  console.timeEnd("Searching a URL");
+
+  const ipNs = await dns.promises.resolve4(nameServers[1]);
+
+  const resolver = new dns.Resolver();
+  resolver.setServers(ipNs);
+
+  console.time("Searching URL by specific DNS");
+  resolver.resolve4(searchedUrl, (error, addressesWithResolver) => {
+    if (error) {
+      console.error("Couldn't resolve DNS");
+      return;
+    }
+    console.timeEnd("Searching URL by specific DNS");
+    console.log(addressesWithResolver);
+  });
+}
+
+bootstrap();
