12a18ec3a736b3c622f394e7c1a83f82b0329055
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-05 11:51:27 -0300

    Dates ex03

--- date.js
+++ date.js
@@ -3,3 +3,6 @@ const { DateTime } = require("luxon");
 const timenow = DateTime.now();
 console.log(timenow);
 console.log(new Date().getMonth()); // starts on month 0
+
+const birthDay = DateTime.fromFormat("1995-12-14", "yyyy-M-dd");
+console.log(birthDay);
